f182477fe3e7ec3b7cc3d6ca2c33096e687209d8
HenryPili <pilihenry89@gmail.com>
2025-02-28 19:18:23 +0800

    commit

--- About.js
+++ About.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     const aboutSection = document.querySelector('.About');
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector("nav ul");
 
     function handleFadeInAnimation() {
-
         const isInViewport = (elem) => {
             const bounding = elem.getBoundingClientRect();
             return (
@@ -21,6 +21,10 @@ document.addEventListener('DOMContentLoaded', function () {
         handleFadeInAnimation();
     });
 
-
     handleFadeInAnimation();
+
+    // Toggle mobile menu
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
 });
